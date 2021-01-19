40fe56cd1fa0638becc6688dd98bc29399352f2d
Wangbz <e18646637590@163.com>
2021-01-19 19:09:58 +0800

    首次添加.

---
+++ src/main/java/com/cls/Lagou01Application.java
@@ -0,0 +1,13 @@
+package com.cls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lagou01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lagou01Application.class, args);
+    }
+
+}
